4355a82a0d00d1452e2b60903139af4efa365cbb
Denise Wayne <waynedenise.n@gmail.com>
2022-11-25 16:17:54 -0800

    add animation and gif

--- julia_set.py
+++ julia_set.py
@@ -5,25 +5,29 @@ import matplotlib.animation as animation
 # This returns a 2d array of complex numbers
 # Pixel Density = # pixels per unit
 def z(xmin, xmax, ymin, ymax, pixel_density):
-    real = np.linspace(xmin, xmax, int((xmax-xmin) * pixel_density))
-    imaginary = np.linspace(ymin, ymax, int((ymax-ymin) * pixel_density))
+    real = np.linspace(xmin, xmax, int((xmax - xmin) * pixel_density))
+    imaginary = np.linspace(ymin, ymax, int((ymax - ymin) * pixel_density))
     return real[np.newaxis, :] + imaginary[:, np.newaxis] * 1j
+
 
 def iterate(z, c, num_iterations):
     for _ in range(num_iterations):
         z = z**2 + c
     return z
 
+
 # Returns the complex_matrix as true/false values if z is in the Mandelbrot set (threshold less than 2)
-def is_stable(z,c, num_iterations):
+def is_stable(z, c, num_iterations):
     for _ in range(num_iterations):
         z = z**2 + c
-    return abs(iterate(z,c,num_iterations)) <= 2
+    return abs(iterate(z, c, num_iterations)) <= 2
+
 
 # Filter out numbers in matrix that are True
 def get_members(z, c, num_iterations):
     mask = is_stable(z, c, num_iterations)
     return z[mask]
+
 
 def get_z(c, num_iterations):
     z = 0
@@ -31,17 +35,18 @@ def get_z(c, num_iterations):
         z = z**2 + c
     return z
 
+
 # Create Visual scatterplot of equation
 def julia_scatterplot(z, c, num_iterations):
     members = get_members(z, c, num_iterations)
-    plot.imshow(is_stable(z, c, num_iterations),  interpolation="bicubic", cmap="magma")
+    plot.imshow(is_stable(z, c, num_iterations), interpolation="bicubic", cmap="magma")
     plot.gca().set_aspect("equal")
     plot.axis("on")
     plot.tight_layout()
     plot.show()
 
+
 # Run
 np.warnings.filterwarnings("ignore")
 matrix = z(-1, 0.25, -0.75, 0.75, pixel_density=512)
-julia_scatterplot(matrix, -0.744 + 0.148j ,20)
-
+julia_scatterplot(matrix, -0.744 + 0.148j, 20)
--- mandelbrot_equation.py
+++ mandelbrot_equation.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plot
+from PIL import Image
 
 # This returns a 2d array of complex numbers
 # Pixel Density = # pixels per unit
 def complex_matrix(xmin, xmax, ymin, ymax, pixel_density):
-    real = np.linspace(xmin, xmax, int((xmax-xmin) * pixel_density))
-    imaginary = np.linspace(ymin, ymax, int((ymax-ymin) * pixel_density))
+    real = np.linspace(xmin, xmax, int((xmax - xmin) * pixel_density))
+    imaginary = np.linspace(ymin, ymax, int((ymax - ymin) * pixel_density))
     return real[np.newaxis, :] + imaginary[:, np.newaxis] * 1j
 
 
@@ -16,21 +17,28 @@ def is_stable(c, num_iterations):
         z = z**2 + c
     return abs(z) <= 2
 
+
 # Filter out numbers in matrix that are True
 def get_members(c, num_iterations):
     mask = is_stable(c, num_iterations)
     return c[mask]
 
+
 # Create Visual scatterplot of equation
 def mandelbrot_scatterplot(c, num_iterations):
     members = get_members(c, num_iterations)
-    plot.scatter(members.real, members.imag, color="black", marker="1", s=1)
+    # plot.scatter(members.real, members.imag, color="black", marker="1", s=1)
+    plot.imshow(is_stable(c, num_iterations), cmap="binary")
     plot.gca().set_aspect("equal")
     plot.axis("on")
     plot.tight_layout()
     plot.show()
 
+
 # Run
 np.warnings.filterwarnings("ignore")
-matrix = complex_matrix(-2, 0.5, -1.5, 1.5, pixel_density=512)
-mandelbrot_scatterplot(matrix, 20)
+matrix = complex_matrix(-2, 0.5, -1.5, 1.5, pixel_density=612)
+image = Image.fromarray(~is_stable(matrix, 20))
+# image.show()
+Image.effect_mandelbrot((512, 512), (-3, -2.5, 2, 2.5), 100).show()
+# mandelbrot_scatterplot(matrix, 20)
